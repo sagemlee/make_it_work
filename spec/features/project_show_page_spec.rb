89af105ad04fbe591b17d4e251cb1240541ceb5e
Sage Lee <sagemcklee@gmail.com>
2020-05-15 10:56:34 -0600

    Add work on the first extenstion

--- spec/features/project_show_page_spec.rb
+++ spec/features/project_show_page_spec.rb
@@ -27,8 +27,25 @@ require "rails_helper"
 
       ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
       ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-      
+
       visit "/projects/#{news_chic.id}"
       expect(page).to have_content("Number of Contestants: 2")
     end
+
+    xit "sees average years of experience" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+      expect(page).to have_content("Average Years of Experience: 12.5")
+    end
+
+
   end
