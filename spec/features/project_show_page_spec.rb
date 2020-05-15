7235dd86c8bba4adb2e9feb8a8edebba4bc99d69
Sage Lee <sagemcklee@gmail.com>
2020-05-15 09:23:32 -0600

    Add project show page spec

---
+++ spec/features/project_show_page_spec.rb
@@ -0,0 +1,17 @@
+require rails_helper
+
+  describe "when visitor goes to project show page" do
+    it "sees project name, material, and theme of project's challege" do
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page)to have_content(news_chic.name)
+      expect(page)to have_content(news_chic.material)
+      expect(page)to have_content(recycled_material_challenge.theme)
+    end
+  end
